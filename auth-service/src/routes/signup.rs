9b6041ccc01e2bf840566705d99e568b8cf10586
Gyanendra Aggarwal <gyanendra.aggarwal@gmail.com>
2024-05-22 20:55:03 -0500

    sprint3 task3

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -1,23 +1,36 @@
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
-use crate::{app_state::AppState, domain::user::User};
+use crate::{app_state::AppState, 
+            domain::{error::AuthAPIError, user::User},
+            services::hashmap_user_store::UserStoreError};
 
 pub async fn signup(State(state): State<AppState>, 
-                    Json(request): Json<SignupRequest>) -> impl IntoResponse {
-// Create a new `User` instance using data in the `request`
+                    Json(request): Json<SignupRequest>) -> 
+                    Result<impl IntoResponse, AuthAPIError> {
+
+    let email = &request.email;
+    let password = &request.password; 
+    println!("email: {} password: {}", email, password);
+
+    if !valid_credentials(email, password) {
+        return Err(AuthAPIError::InvalidCredentials);
+    }   
+
     let user = request.create_user();
 
     let mut user_store = state.user_store.write().await;
 
-// TODO: Add `user` to the `user_store`. Simply unwrap the returned `Result` enum type for now.
-    user_store.add_user(user).unwrap();
+    let result = user_store.add_user(user);
+    match result {
+        Ok(_) => Ok((StatusCode::CREATED, Json(SignupResponse {
+            message: "User created successfully!".to_string(),
+        }))),
+        Err(UserStoreError::UserAlreadyExists) => Err(AuthAPIError::UserAlreadyExists),
+        Err(UserStoreError::UnexpectedError) => Err(AuthAPIError::UnexpectedError),
+        Err(UserStoreError::InvalidCredentials) => Err(AuthAPIError::InvalidCredentials),
+        Err(UserStoreError::UserNotFound) => Err(AuthAPIError::UnexpectedError)
+     }
 
-    let response = Json(SignupResponse {
-        message: "User created successfully!".to_string(),
-    });
-
-    (StatusCode::CREATED, response)
-    //    StatusCode::OK.into_response()
 }
 
 #[derive(Deserialize)]
@@ -38,4 +51,23 @@ impl SignupRequest {
 #[derive(Serialize, Debug, Deserialize, PartialEq)]
 pub struct SignupResponse {
     pub message: String,
+}
+
+fn valid_password(password: &str, len: usize) -> bool {
+    password.chars().count() >= len
+}
+
+fn valid_email(email: &str, ch: char) -> bool {
+    !email.is_empty() && {
+        let fvalue = email.find(ch);
+        match fvalue {
+            None => false,
+            Some(0) => false,
+            Some(_) => true,
+        }
+    }
+}
+
+fn valid_credentials(email: &str, password: &str) -> bool {
+    valid_email(email, '@') && valid_password(password, 8)
 }
--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,6 +1,6 @@
 
 use crate::helpers::TestApp;
-use auth_service::routes::SignupResponse;
+use auth_service::{routes::SignupResponse, ErrorResponse};
 
 #[tokio::test]
 async fn should_return_201_if_valid_input() {
@@ -53,4 +53,69 @@ async fn should_return_422_if_malformed_input() {
             test_case
         );
     }
+}
+/*
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    // The signup route should return a 400 HTTP status code if an invalid input is sent.
+    // The input is considered invalid if:
+    // - The email is empty or does not contain '@'
+    // - The password is less than 8 characters
+
+    // Create an array of invalid inputs. Then, iterate through the array and 
+    // make HTTP calls to the signup route. Assert a 400 HTTP status code is returned.
+    let app = TestApp::new().await;
+    let test_cases = [
+        serde_json::json!({
+            "email": "",
+            "password": "password123",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "email": "example.com",
+            "password": "password123",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "email": "my.name@example.com",
+            "password": "1234",
+            "requires2FA": true
+        })
+    ];
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await; // call `post_signup`
+        assert_eq!(
+            response
+              .json::<ErrorResponse>()
+              .await
+              .expect("Could not deserialize response body to ErrorResponse")
+              .error,
+              "Invalid credentials".to_owned()
+  
+        );
+    }   
+}
+*/
+#[tokio::test]
+async fn should_return_409_if_email_already_exists() {
+    // Call the signup route twice. The second request should fail with a 409 HTTP status code    
+    let app = TestApp::new().await;
+    let test_case1 = serde_json::json!({
+        "email": "my.name@example.com",
+        "password": "password123",
+        "requires2FA": true
+    });
+
+    let test_case2 = test_case1.clone();
+    let _ = app.post_signup(&test_case1).await;
+    let response = app.post_signup(&test_case2).await;
+    assert_eq!(
+        response
+            .json::<ErrorResponse>()
+            .await
+            .expect("Could not deserialize response body to ErrorResponse")
+            .error,
+        "User already exists".to_owned()
+    );
+
 }
